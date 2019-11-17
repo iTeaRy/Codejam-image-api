1dd7a4c20c815c8d0e91575b1a6ef9c627135bb2
OlegRudevich <olegrudevich19@gmail.com>
2019-11-18 02:33:02 +0300

    feat: add post request

--- authorizing.js
+++ authorizing.js
@@ -2,23 +2,14 @@ const authorizingBtn = document.getElementById('authorizing');
 const clienId = 'b55df3b0d2e53b36ec7c';
 const clientSecret = '6b9f7f50d4618c19d0dd6c758604e46e3b4f0c95';
 const callback = 'https://iteary.github.io/Codejam-image-api';
-netlifyIdentity.open();
-
-// Get the current user:
-const user = netlifyIdentity.currentUser();
-
-// Bind to events
-netlifyIdentity.on('init', user => console.log('init', user));
-netlifyIdentity.on('login', user => console.log('login', user));
-netlifyIdentity.on('logout', () => console.log('Logged out'));
-netlifyIdentity.on('error', err => console.error('Error', err));
-netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
-
-// Close the modal
-netlifyIdentity.close();
-
-// Log out the user
-netlifyIdentity.logout();
-
+const anchorTag = document.getElementById('login')
+const outputText = document.getElementById('output')
+anchorTag.addEventListener('click', (e) => {
+    e.preventDefault()
+    const authenticator = new netlify.default ({})
+    authenticator.authenticate({provider:"github", scope: "user"}, (err, data) => {
+        err ? outputText.innerText = "Error Authenticating with GitHub: " + err :
+            outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token
+    })
+})
 
