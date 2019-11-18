4606478e8fac1086f47707b5f5a25e786f954e80
OlegRudevich <olegrudevich19@gmail.com>
2019-11-18 03:26:23 +0300

    fix: post request

--- authorizing.js
+++ authorizing.js
@@ -4,21 +4,24 @@ const clientSecret = '6b9f7f50d4618c19d0dd6c758604e46e3b4f0c95';
 const callback = 'https://iteary.github.io/Codejam-image-api';
 const anchorTag = document.getElementById('login');
 const outputText = document.getElementById('output');
-let response = '';
 anchorTag.addEventListener('click', (e) => {
     e.preventDefault()
     const authenticator = new netlify.default ({})
     authenticator.authenticate({provider:"github", scope: "user"}, (err, data) => {
         err ? outputText.innerText = "Error Authenticating with GitHub: " + err :
-            outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token;
-        response = fetch('https://api.github.com/user',
-            {
-
-                headers: {
-                    'Authorization': ' token OAUTH-TOKEN',
-                    ...
-                }});
-        console.log(response);
-    })
+            outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token
+    });
+    getResponse();
 });
 
+
+
+async function getResponse() {
+    const resp =  await fetch('https://api.github.com/user',
+        {
+            headers: {
+                'Authorization': ' token OAUTH-TOKEN',
+            }
+        });
+    console.log(resp);
+}
