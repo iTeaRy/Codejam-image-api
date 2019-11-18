12281774e27c1edcf6e1b96a4d646103e6abf6fc
OlegRudevich <olegrudevich19@gmail.com>
2019-11-18 17:11:32 +0300

    fix: get request

--- authorizing.js
+++ authorizing.js
@@ -3,15 +3,14 @@ const clienId = 'b55df3b0d2e53b36ec7c';
 const clientSecret = '6b9f7f50d4618c19d0dd6c758604e46e3b4f0c95';
 const callback = 'https://iteary.github.io/Codejam-image-api';
 const anchorTag = document.getElementById('login');
-const outputText = document.getElementById('output');
+const outputUser = document.getElementById('user');
 anchorTag.addEventListener('click', (e) => {
     e.preventDefault()
     const authenticator = new netlify.default ({})
     authenticator.authenticate({provider:"github", scope: "user"}, (err, data) => {
         if (err) {
-            outputText.innerText = "Error Authenticating with GitHub: " + err;
+            alert(`Error Authenticating with GitHub: ${err}`);
         } else {
-            outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token;
             getResponse(data.token);
         }
 
@@ -19,7 +18,9 @@ anchorTag.addEventListener('click', (e) => {
 
 });
 
-
+function setUserData (data) {
+    outputUser.innerHTML = `You login as ${data.login}`;
+}
 
 async function getResponse(token) {
     const resp =  await fetch('https://api.github.com/user',
@@ -29,5 +30,5 @@ async function getResponse(token) {
             }
         });
     let data = await resp.json();
-    console.log(data.login, 'data');
+    setUserData(data);
 }
